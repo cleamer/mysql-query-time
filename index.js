bb54bcce0310a669cce035d55ed168f93e1464ef
cloer <chltjrrb97@gmail.com>
2022-02-18 08:43:36 -0500

    FIX: connection names

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ require('dotenv').config({ path: path.join(process.cwd(), '.env') });
 const mysql = require('mysql2/promise');
 
 exports.createTest = async () => {
-  const userConnection = await mysql.createConnection({
+  const queryConnection = await mysql.createConnection({
     host: process.env.TESTER_HOST,
     user: process.env.TESTER,
     password: process.env.TESTER_PASSWORD,
     database: process.env.TEST_DATABASE,
   });
-  const masterConnection = await mysql.createConnection({
+  const timeConnection = await mysql.createConnection({
     host: process.env.TESTER_HOST,
     user: process.env.TESTER,
     password: process.env.TESTER_PASSWORD,
@@ -19,18 +19,18 @@ exports.createTest = async () => {
   });
   return async (query, errorHandler = (err) => console.error(err)) => {
     try {
-      const [queryResult] = await userConnection.query(query);
-      const [timeResult] = await masterConnection.query(
+      const [queryResult] = await queryConnection.query(query);
+      const [timeResult] = await timeConnection.query(
         `SELECT EVENT_ID, TRUNCATE(TIMER_WAIT/1000000000000,6) as Duration, SQL_TEXT
       FROM performance_schema.events_statements_history_long WHERE SQL_TEXT like ?;`,
         query
       );
-      userConnection.end();
-      masterConnection.end();
+      queryConnection.end();
+      timeConnection.end();
       return { queryResult, timeResult };
     } catch (err) {
-      userConnection.end();
-      masterConnection.end();
+      queryConnection.end();
+      timeConnection.end();
       errorHandler(err);
       return;
     }
